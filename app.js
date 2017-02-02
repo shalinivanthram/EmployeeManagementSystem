782d97729833e68bff92328bbb91ed030601c865
bipneswar <bmangara@in.ibm.com>
2017-02-02 11:45:59 +0530

    BodyParser implemented for POST request

--- app.js
+++ app.js
@@ -8,52 +8,37 @@
 // for more info, see: http://expressjs.com
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-var engines = require('consolidate');
-
-var loginRouter = express.Router();
 var login = require("./Controller/login");
 
 // create a new express server
 var app = express();
-
+var router = express.Router();
 // serve the files out of ./public as our main files
 app.use(express.static(path.join(__dirname ,'public')));
 //Shalini
 app.set('views', path.join(__dirname, '/views')); 
-app.engine('html', engines.mustache);
-app.set('view engine', 'html');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //render login.html
 app.get('/login', function(req,res){
-	res.render("login.html");
+	res.render("login.ejs");
 });
 app.post('/login', function(req,res,next){
-	console.log("test: " + JSON.stringify(req.body));
-	//login.getList(req,res);
+    login.getList(req,res);
 });
 
 app.get('/listEmp', function(req,res){
-	res.render("ListEmployees.html");
+	res.render("ListEmployees.ejs");
 });
-
-//navigate from login
-loginRouter.route("/")
-	.get(function(req, res){
-		console.log('login router called');
-		//res.send("login success");
-		res.render("ListEmployees.html");
-	});
-loginRouter.route("/admin")
-	.get(function(req, res){
-		console.log('admin login router called');
-		res.send("admin login success");
-	});
-app.use('/AppLogin', loginRouter);
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
@@ -63,3 +48,4 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
